3c65adb50956055640b3c2548be5849da37864ea
LuckyHookin <luckyhookin@qq.com>
2022-02-03 17:34:38 +0800

    init

---
+++ dist/res/index.js
@@ -0,0 +1,53 @@
+let fs=require('fs');
+const { fdir } = require("fdir");
+
+console.log(process.argv);
+
+
+const dir = process.argv[2];
+// const dir = 'E:\\data\\web\\vue.js\\vue3\\hir';
+
+// 文件类型过滤
+const filterArg = process.argv[3];
+// const filterArg = '\\.js|\\.vue';
+const filter = new RegExp("("+filterArg+")$",'i');
+
+// 文件夹过滤
+const excludeArg = process.argv[4];
+// const excludeArg = 'node_modules|dist';
+const exclude = new RegExp(excludeArg,'i');
+
+const codeFilePath = process.argv[5];
+// const codeFilePath = './code.txt';
+
+try {
+    fs.unlinkSync(codeFilePath);
+    console.log(`reset ${codeFilePath}`);
+} catch (error) {
+    
+}
+
+let api = new fdir().withFullPaths().filter(
+    (path, isDirectory) => {
+        // console.log(path);
+        // 文件类型过滤, true 则包含
+        return filter.test(path);
+    }
+).exclude((dirName, dirPath) =>{
+    // console.log(dirName);
+    return exclude.test(dirName);
+}
+);
+
+api.crawl(dir).withPromise().then((files) => {
+    for (let index = 0; index < files.length; index++) {
+        const filePath = files[index];
+        console.log(index+1,filePath);
+        const data = fs.readFileSync(filePath);
+        const content = data.toString();
+        let replaceContent = content.replace(/\s*(\/\*.*?\*\/|<!--.*?-->|\/\/.*?$)/gms, '');
+        replaceContent = replaceContent.replace(/^\s*\n/gm, '');
+        fs.appendFileSync(codeFilePath, replaceContent);   
+    }
+    console.log('ok!');
+});
